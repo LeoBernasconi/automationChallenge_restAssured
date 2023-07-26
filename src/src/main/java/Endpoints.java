44970bf85dda3e1facf4c48c1dac92c945a31cbf
LeoBernasconi <47478679+LeoBernasconi@users.noreply.github.com>
2023-07-26 11:45:56 -0400

    Update Endpoints.java
    Fixing problems after testing the APIs.

--- src/src/main/java/Endpoints.java
+++ src/src/main/java/Endpoints.java
@@ -4,6 +4,8 @@ import static io.restassured.RestAssured.given;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
 public class Endpoints {
@@ -14,7 +16,7 @@ public class Endpoints {
 	//Endpoint 1: Get Comments by article Id
     public static Boolean doesTheArticleHaveComments(int articleId) throws IOException {
     	//Define the endpoint config
-		URL url = new URL(base_url + "articles/" + articleId + "/comments");
+		URL url = new URL(base_url + "articles/" + articleId + "/comments?order=desc&sort=creation&site=stackoverflow");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("Accept", "application/json");
@@ -33,26 +35,26 @@ public class Endpoints {
 	//Endpoint 2: Get user name by user id
     public static String getUserNameByUserId(int userId) throws IOException {
     	//Define the endpoint config
-		URL url = new URL(base_url + "users/" + userId);
+		URL url = new URL(base_url + "users/" + userId + "?order=desc&sort=creation&site=stackoverflow");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("Accept", "application/json");
 		//Call the endpoint and get the user name
 		Response response = given().get(url).then().extract().response();
-		String userName = response.path("items.display_name");
+		String userName = response.path("items.get(0).display_name");
 		return userName;
     }
     
 	//Endpoint 3: Get Article information by article id
     public static int getArticleInformationById(int articleId) throws IOException {
     	//Define the endpoint config
-		URL url = new URL(base_url + "articles/" + articleId);
+		URL url = new URL(base_url + "articles/" + articleId + "?order=desc&sort=creation&site=stackoverflow");
 		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
 		conn.setRequestMethod("GET");
 		conn.setRequestProperty("Accept", "application/json");
 		//Call the endpoint
 		Response response = given().get(url).then().extract().response();
-		int userId = Integer.parseInt(response.path("items.owner.user_id"));
+		int userId = response.path("items.get(0).owner.user_id");
 		return userId;
     }
 
